cb57cbed5ac30c65ca7d2833b87bbb7e5cf5ffd6
masonn99 <masonnaung2023@u.northwestern.edu>
2024-04-20 03:02:13 -0700

    added scrollables

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import SearchBar from "./components/SearchBar";
 import Header from "./components/Header";
-
-
-
-
+import Footer from "./components/Footer";
+import "./styles/App.css";
 
 function App() {
   return (
-  
     <div>
-      <Header />
-      <SearchBar />
+      <div className="fixed-header">
+        <Header />
+        <SearchBar />
+      </div>
+      <Footer /> 
+      
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/Footer.css"; // Import the new CSS file
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>All rights reserved</p>
+    </div>
+  );
+}
+
+export default Footer;
