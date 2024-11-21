c5b0d5e4bde1e058c7580ef88101c4137adfae2d
Mason Ng <mnaung@expediagroup.com>
2024-11-20 22:14:38 -0800

    add vercel insights

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import SearchBar from "./components/SearchBar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./styles/App.css";
-
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-900 text-zinc-100">
+    <div>
       <Analytics />
-      <Header />
-      <main className="pt-1 pb-16"> {/* Add padding to account for fixed header and footer */}
+      <SpeedInsights />
+      <div className="fixed-header">
+        <Header />
         <SearchBar />
-      </main>
+      </div>
       <Footer />
     </div>
   );
